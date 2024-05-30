84caa9401e4e3f68ce19760efd6aa60575e22c4b
Daniel <daniel.laguarta@gmail.com>
2024-05-29 23:44:24 -0500

    photos now showing

--- client/src/components/ThoughtList/index.jsx
+++ client/src/components/ThoughtList/index.jsx
@@ -1,4 +1,4 @@
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 const ThoughtList = ({
   thoughts,
@@ -23,13 +23,13 @@ const ThoughtList = ({
                   to={`/profiles/${thought.thoughtAuthor}`}
                 >
                   {thought.thoughtAuthor} <br />
-                  <span style={{ fontSize: '1rem' }}>
+                  <span style={{ fontSize: "1rem" }}>
                     had this thought on {thought.createdAt}
                   </span>
                 </Link>
               ) : (
                 <>
-                  <span style={{ fontSize: '1rem' }}>
+                  <span style={{ fontSize: "1rem" }}>
                     You had this thought on {thought.createdAt}
                   </span>
                 </>
@@ -37,6 +37,9 @@ const ThoughtList = ({
             </h4>
             <div className="card-body bg-light p-2">
               <p>{thought.thoughtText}</p>
+              <div>
+                <img src={thought.url} height="200" />
+              </div>
             </div>
             <Link
               className="btn btn-primary btn-block btn-squared"
