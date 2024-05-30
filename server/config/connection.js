a47b3b58a3645f264ddb0121fe763a6327fcf611
mylesjav <152746324+mylesjav@users.noreply.github.com>
2024-05-29 19:48:56 -0700

    add followers to schema

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
 
-// mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/programming-thoughts');
-mongoose.connect("mongodb+srv://smyles808:sIQymf5qi4tv0Dli@tech-thoughts.eyehbhe.mongodb.net/tech-thoughts?retryWrites=true&w=majority");
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/programming-thoughts');
+
 module.exports = mongoose.connection;
