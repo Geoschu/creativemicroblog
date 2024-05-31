4b8096d5b90c2243c911b953daa4be09e6779ba1
mylesjav <152746324+mylesjav@users.noreply.github.com>
2024-05-30 19:21:44 -0700

    moved followers to mutations

--- client/src/components/ThoughtForm/index.jsx
+++ client/src/components/ThoughtForm/index.jsx
@@ -99,4 +99,58 @@ const ThoughtForm = () => {
   );
 };
 
+const FOLLOW_USER = gql`
+  mutation FollowUser($id: ID!) {
+    followUser(id: $id) {
+      id
+      name
+      email
+      followers
+    }
+  }
+`;
+
+const UNFOLLOW_USER = gql`
+  mutation UnfollowUser($id: ID!) {
+    unfollowUser(id: $id) {
+      id
+      name
+      email
+      followers
+    }
+  }
+`;
+
+function UserProfile({ userId }) {
+  const [followUser] = useMutation(FOLLOW_USER);
+  const [unfollowUser] = useMutation(UNFOLLOW_USER);
+
+  const handleFollow = async () => {
+    try {
+      const { data } = await followUser({ variables: { id: userId } });
+      console.log(data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const handleUnfollow = async () => {
+    try {
+      const { data } = await unfollowUser({ variables: { id: userId } });
+      console.log(data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <div>
+      {/* Render user profile */}
+      <button onClick={handleFollow}>Follow</button>
+      <button onClick={handleUnfollow}>Unfollow</button>
+    </div>
+  );
+}
+
+
 export default ThoughtForm;
--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -111,3 +111,5 @@ function UserProfile({ userId }) {
   );
 }
 
+
+
