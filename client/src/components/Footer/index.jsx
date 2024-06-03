037cf5919d6d3258ca8009cbf4aa0053f9bec27c
George Schultz <georgeschultz@outlook.com>
2024-06-03 00:31:37 -0700

    fixed footer glitches

--- client/src/components/Footer/index.jsx
+++ client/src/components/Footer/index.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
   const location = useLocation();
   const navigate = useNavigate();
   return (
-    <footer className="w-100 mt-auto bg-secondary p-4">
+    <footer className="w-100 mt-auto bg-secondary p-4 footer">
       <div className="container text-center mb-5">
         {location.pathname !== "/" && (
           <button className="btn btn-dark mb-3" onClick={() => navigate(-1)}>
