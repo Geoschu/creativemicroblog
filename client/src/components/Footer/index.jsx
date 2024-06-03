5a7740e0e0636aa4704a46da8154feb34e11d1d4
George Schultz <georgeschultz@outlook.com>
2024-06-03 02:04:05 -0700

    made heades smaller

--- client/src/components/Footer/index.jsx
+++ client/src/components/Footer/index.jsx
@@ -4,29 +4,17 @@ const Footer = () => {
   const location = useLocation();
   const navigate = useNavigate();
   return (
-    <footer className="w-100 mt-auto bg-secondary p-4 footer">
+    <footer className="w-full mt-auto bg-secondary p-4">
       <div className="container text-center mb-5">
         {location.pathname !== "/" && (
           <button className="btn btn-dark mb-3" onClick={() => navigate(-1)}>
             &larr; Go Back
           </button>
         )}
-        <div style={{ display: "flex", justifyContent: "center" }}>
-          <header
-            className="text-white bg-gradient-to-r from-green-400 via-blue-500 to-purple-600 p-5 text-center fixed bottom-0 w-full"
-            style={{
-              justifyContent: "center",
-              zIndex: 2,
-            }}
-          >
-            <div
-              style={{
-                display: "flex",
-                flexDirection: "column",
-                alignItems: "center",
-              }}
-            >
-              <h1 className="text-4xl font-bold tracking-wide">
+        <div className="flex justify-center">
+          <header className="text-white bg-gradient-to-r from-green-400 via-blue-500 to-purple-600 p-1 text-center fixed bottom-0 w-full z-10">
+            <div className="flex flex-col items-center">
+              <h1 className="text-3xl font-bold tracking-wide">
                 welcome to PixLit
                 <span className="ml-2 text-xl">Find your inspiration here</span>
               </h1>
@@ -48,7 +36,7 @@ const Footer = () => {
               alt="Three small children in a trenchcoat"
               width="40"
               height="40"
-              style={{ marginLeft: "10px" }} // Add some space between the text and the image
+              className="ml-2" // Add some space between the text and the image
             />
           </header>
         </div>
