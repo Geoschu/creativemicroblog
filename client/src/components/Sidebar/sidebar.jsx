01223a5a32b1de1bf35bf36e7510a5768d040aa4
George Schultz <georgeschultz@outlook.com>
2024-06-02 20:31:29 -0700

    fixed logo placement on sidebar

--- client/src/components/Sidebar/sidebar.jsx
+++ client/src/components/Sidebar/sidebar.jsx
@@ -27,17 +27,20 @@ const Sidebar = () => {
 
   return (
     <div
-      className={`sidebar flex flex-col justify-center items-center ${
+      className={`sidebar flex flex-col justify-start items-center ${
         !isHeaderVisible ? "full-height" : ""
       }`}
     >
-      {!isHeaderVisible && (
-        <img
-          src={logo}
-          alt="Logo"
-          style={{ width: "75%", height: "auto", padding: "10px" }}
-        />
-      )}
+      <img
+        src={logo}
+        alt="Logo"
+        style={{
+          width: "75%",
+          height: "auto",
+          padding: "10px",
+          display: isHeaderVisible ? "none" : "block",
+        }}
+      />
       {Auth.loggedIn() ? (
         <>
           <Link className="btn btn-lg btn-info m-2" to="/me">
@@ -56,11 +59,6 @@ const Sidebar = () => {
           <Link className="btn btn-lg btn-info m-2" to="/login">
             <span className="text-white font-bold py-2 px-4 rounded bg-green-500 hover:bg-blue-700">
               Login
-            </span>
-          </Link>
-          <Link className="btn btn-lg btn-light m-2" to="/signup">
-            <span className="text-black font-bold py-2 px-4 rounded bg-gray-300 hover:bg-gray-400">
-              Signup
             </span>
           </Link>
         </>
